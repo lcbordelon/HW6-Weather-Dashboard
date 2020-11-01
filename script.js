4788a12f8c325d12e0e96352a1d4f88618ae3040
Lindsay Bordelon <lcbordelon@gmail.com>
2020-10-31 19:15:04 -0600

    updated element definition

--- script.js
+++ script.js
@@ -13,8 +13,10 @@ var get5Day = function (event) {
     .then((response) => response.json())
     .then((data) => console.log(data));
 
-  var tempEl = fivedayDisplay.createElement("<p>");
-  tempEl.textContent = response.list[0].main.temp;
+  var tempEl = document.createElement("p");
+  tempEl.textContent = data.list[0].main.temp;
+
+  document.getElementById("Day1Card").appendChild(tempEl);
   //loop through 5 day weather data to compile 5 day weather card
   // for (var i = 0; i < 5; i++) {
   //create element to display temperature
